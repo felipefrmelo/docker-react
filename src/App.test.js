f7aaeab7a619534a48267aeb99adcb3fcb555a5e
Felipe <felipefrmelo@gmail.com>
2020-05-06 18:57:57 -0300

    add fase deploy

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/oieeeolosalosa/i);
+  const linkElement = getByText(/oieeeolosalos/i);
   expect(linkElement).toBeInTheDocument();
 });
