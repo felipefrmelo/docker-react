79b33e84659afccd7efbb3b8b4958a944f2fadce
Felipe <felipefrmelo@gmail.com>
2020-05-06 18:30:46 -0300

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/2121212/i);
+  const linkElement = getByText(/oieeeolosalosa/i);
   expect(linkElement).toBeInTheDocument();
 });
